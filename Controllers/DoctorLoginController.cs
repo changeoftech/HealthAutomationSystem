fba13b0ec2f39c33c693c6685c23d06f9225bdbd
changeoftech <changeoftech@gmail.com>
2020-08-11 14:47:49 +0300

    eklendi.

---
+++ Controllers/DoctorLoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HealthAutomationSystem.Controllers
+{
+    public class DoctorLoginController : Controller
+    {
+        // GET: DoktorLogin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HealthAutomationSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            /// Hasta bilgilerini güncelleyecebilecek.
+            /// Hasta randevularını görebilecek.
+            /// Hasta reçetelerini görebilecek.
+            /// Doktor randevularını görebilecek.
+            /// Doktor reçete yazabilecek.
+            /// Doktor reçeteleri görebilecek.
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
